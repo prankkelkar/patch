5caf6237064a7c2ebb67b14e6921547bc8688325
Prasanna Kelkar <prankkelkar@gmail.com>
2019-08-02 14:46:07 +0530

    Update script.sh

--- git/patch/script.sh
+++ git/patch/script.sh
@@ -1,7 +1,13 @@
 cat >patch_install_sealed_secretes <<-"EOF"
-@@ -5,4 +5,4 @@ release=$(curl -sI https://github.com/bitnami-labs/sealed-secrets/releases/lates
+@@ -1,8 +1,8 @@
+ #!/bin/bash
 
- echo "SealedSecrets release: $release"
+-release=$(curl -sI https://github.com/bitnami-labs/sealed-secrets/releases/latest | grep Location | awk -F"/" '{ printf "%s", $NF }' | tr -d '\r')
++#release=$(curl -sI https://github.com/bitnami-labs/sealed-secrets/releases/latest | grep Location | awk -F"/" '{ printf "%s", $NF }' | tr -d '\r')
+ #release=$(curl --silent "https://api.github.com/repos/bitnami-labs/sealed-secrets/releases/latest" | sed -n 's/.*"tag_name": *"\([^"]*\)".*/\1/p')
+
+-echo "SealedSecrets release: $release"
++#echo "SealedSecrets release: $release"
 
 -helm install --namespace kube-system --name ofc-sealedsecrets stable/sealed-secrets
 \ No newline at end of file
